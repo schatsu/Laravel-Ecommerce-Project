738ff8dedc0d511fcaa74ae8c4fb00624e1cefdd
schatsu <tahasametsatsu@gmail.com>
2025-12-25 18:53:29 +0300

    General update to payment system

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -20,6 +20,7 @@ return Application::configure(basePath: dirname(__DIR__))
         
         $middleware->validateCsrfTokens(except: [
             'odeme/callback',
+            'odeme/3d-callback',
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
